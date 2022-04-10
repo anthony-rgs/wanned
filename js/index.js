67130b665d6dd1222a3b5477d29ddca88cd214b9
Anthony-Rgs <95974061+Anthony-Rgs@users.noreply.github.com>
2022-04-11 00:52:25 +0200

    feat: don't show story if already visited

--- js/index.js
+++ js/index.js
@@ -2,32 +2,31 @@ import Game from './classes/Game.js'
 import Story from './classes/Story.js'
 
 const story = new Story(
-  [
-    {
-      text: 'Il était une fois dans un Pays lointain, très lointain, une Principauté appelé Quimbroisie.',
-      duration: 4000,
-    },
-    {
-      text: 'Dans cette principauté régnait en despote un affreux personnage aussi médisant qu’arrogant dont la méchanceté n’égalait que sa médiocrité.',
-      duration: 7000,
-    },
-    {
-      text: 'Il était si gros qu’assis sur son trône seul son énorme tête restait visible aux yeux de ses sujets. Le ténébreux ***.',
-      duration: 7000,
-    },
-    {
-      text: 'Les vassaux de sa cours n’étaient pas particulièrement enthousiaste à l’idée de devoir supporter ce petit chef de pacotille. À la tête de cette révolte, le valeureux Pépin-trois-pomme! Aussi appelé “Baptiste”.',
-      duration: 10000,
-    },
-    {
-      text: 'Soutenue par le sorcier de la cours adepte de magie noir, le valeureux Tisbron, Pépin-trois-pomme va se faufiler à l’intérieur du donjon de l’affreux *** afin de lui couper son énorme tête.',
-      duration: 9000,
-    },
-  ],
-  3000,
+    [{
+            text: 'Il était une fois dans un Pays lointain, très lointain, une Principauté appelé Quimbroisie.',
+            duration: 4000,
+        },
+        {
+            text: 'Dans cette principauté régnait en despote un affreux personnage aussi médisant qu’arrogant dont la méchanceté n’égalait que sa médiocrité.',
+            duration: 7000,
+        },
+        {
+            text: 'Il était si gros qu’assis sur son trône seul son énorme tête restait visible aux yeux de ses sujets. Le ténébreux ***.',
+            duration: 7000,
+        },
+        {
+            text: 'Les vassaux de sa cour n’étaient pas particulièrement enthousiaste à l’idée de devoir supporter ce petit chef de pacotille. À la tête de cette révolte, le valeureux Pépin-trois-pomme! Aussi appelé “Baptiste”.',
+            duration: 10000,
+        },
+        {
+            text: 'Soutenue par le sorcier de la cour adepte de magie noir, le valeureux Tisbron, Pépin-trois-pomme va se faufiler à l’intérieur du donjon de l’affreux *** afin de lui couper son énorme tête.',
+            duration: 9000,
+        },
+    ],
+    3000,
 )
 
-story.disabled = false
+story.disabled = localStorage.getItem('story-finish') != null
 
 story.start()
 
@@ -37,15 +36,16 @@ const fog = document.querySelector('#fog')
 const light = document.querySelector('#light')
 
 story.onEnd(() => {
-  wanned.draw()
-  fog.classList.remove('hidden')
-  light.classList.remove('hidden')
-  storySection.classList.add('hidden')
-  setTimeout(() => {
-    storySection.remove()
-  }, 500)
+    wanned.draw()
+    fog.classList.remove('hidden')
+    light.classList.remove('hidden')
+    storySection.classList.add('hidden')
+    setTimeout(() => {
+        storySection.remove()
+        localStorage.setItem('story-finish', true)
+    }, 500)
 })
 
 window.addEventListener('resize', () => {
-  wanned.updateCanvas()
+    wanned.updateCanvas()
 })
