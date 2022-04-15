c189bc729cf354a36053099bdc1dea4480d96c55
Arthur Fontaine <0arthur.fontaine@gmail.com>
2022-04-15 23:23:02 +0200

    refactor: include Tiled files

---
+++ tiled/Dungeon_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Dungeon_Tileset" tilewidth="16" tileheight="16" tilecount="100" columns="10">
+ <image source="Dungeon_Tileset.png" trans="003039" width="160" height="160"/>
+</tileset>
---
+++ tiled/collision-door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="collision-door" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="collision-door.png" trans="003039" width="16" height="16"/>
+</tileset>
