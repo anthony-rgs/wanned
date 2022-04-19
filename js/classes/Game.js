51dd6cda6df936c525f3af366fa498e7da067667
Cammyxalot <renoir.fab@outlook.com>
2022-04-19 09:42:15 +0200

    feat: menu

--- js/classes/Game.js
+++ js/classes/Game.js
@@ -26,6 +26,7 @@ import mapSpikes from '../../assets/resources/mapSpikes.js'
 import Spikes from './elements/spikes.js'
 import BubbleMaker from './BubbleMaker.js'
 import GameOver from './GameOver.js'
+import Menu from './Menu.js'
 
 class Game {
   constructor(canvas) {
---
+++ js/classes/Menu.js
@@ -0,0 +1,18 @@
+class Menu{
+    constructor(){
+    this.title = document.querySelector(".title")
+    this.launch = document.querySelector(".launch")
+    this.settings = document.querySelector(".settings")
+    this.credits = document.querySelector(".credits")
+    this.quit = document.querySelector(".quit")
+    }
+}
+//---------------------------------------------------
+
+// function aie(){
+//     console.log("aie)")
+// }
+
+// Menu.title.addEventListener('click',aie())
+
+export default Menu;
